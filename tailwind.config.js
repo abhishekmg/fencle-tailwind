468c22d1e6efd3a4a5be2eaba1525c6fcae929ab
abhishekmg <abhishekmg.12@gmail.com>
2020-07-24 21:49:49 +0530

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'leap-blue': '#443EFF',
-        'leap-blue-1' : '#1D1A6E',
-        'leap-blue-2' : '#1CA0F2',
-        'leap-blue-3' : '#817DFF;',
-        'leap-gray': '#F5F7F9',
-        'leap-gray-1' : '#888897',
-        'leap-gray-2' : '#565660',
-        'leap-black' : '#06051F',
-        'leap-pink' : '#FFE2E8'
+        'my-gray' : '#F1F1F1',
+        'my-gray-2' : '#818386',
+        'my-blue' : '#0099DA'
       }
     },
   },
