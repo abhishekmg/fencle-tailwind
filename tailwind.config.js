0a348a33322238519090644628f1864c179f99c6
abhishekmg <abhishekmg.12@gmail.com>
2020-07-27 19:32:59 +0530

    started table

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
       colors: {
         'my-gray' : '#F1F1F1',
         'my-gray-2' : '#818386',
+        'my-gray-3' : '#E6E6E6',
+        'my-gray-4' : '#F7F7F7',
+        'my-gray-5' : '#EAEAEA',
+        'my-gray-6': '#F2F3F4', 
+        'my-blue' : '#0099DA',
+        'my-black' : '#313335',
+        'my-black-2' : '#08080B',
+        'my-green' : '#1ABB97',
         'my-blue' : '#0099DA'
       }
     },
